d049b73afa994bdc7bd463c28b8b314e42cdd4d7
EzVortex <50105426+EzVortex@users.noreply.github.com>
2019-10-12 21:42:02 +0400

    Night theme rework

--- Daniil Dorofeev/js/script.js
+++ Daniil Dorofeev/js/script.js
@@ -8,10 +8,32 @@ $('.menu').hover(function() {
 	});
 });
 
-$('.night-on').click(function(event) {
-	$('body').css('background-color', '#222');
-	$('h1, i, p, a').css('color', '#ddd');
-	$('.block, footer').css('background-color', '#303030');
-	$('.container img').css('border', '2px solid #ddd');
-	$('div').removeClass('.night-on');
+var currentTheme = 1;
+
+$('.theme').click(function(event) {
+	if(currentTheme == 1) {
+		$('body').css('background-color', '#222');
+		$('h1, i, p, a').css('color', '#ddd');
+		$('.block, footer').css('background-color', '#303030');
+		$('.container img').css('border', '2px solid #ddd');
+		$('.theme p').html('Дневная тема');
+		$('.theme i').removeClass('far fa-moon');
+		$('.theme i').addClass('fas fa-sun');
+		$('.theme').css('border', '2px solid #ddd');
+	}
+	if(currentTheme == 2) {
+		$('body').css('background-color', '#f5f5f5');
+		$('h1, i, p, a').css('color', '#222');
+		$('.block, footer').css('background-color', 'white');
+		$('.container img').css('border', '2px solid #ddd');
+		$('.theme p').html('Ночная тема');
+		$('.theme i').removeClass('fas fa-sun');
+		$('.theme i').addClass('far fa-moon');
+		$('.theme').css('border', '2px solid #222');
+	}
+	if(currentTheme == 1) {
+		currentTheme = 2;
+	} else {
+		currentTheme = 1;
+	}
 });
