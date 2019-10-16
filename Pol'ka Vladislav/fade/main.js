091d3357f4c66ffc3fb7b971023a55332152f0d9
sandiiq <sandmon.play@mail.ru>
2019-10-16 19:14:51 +0300

    Delete Site

--- Pol'ka Vladislav/fade/main.js
+++
@@ -1,6 +0,0 @@
-var headerBg = document.getElementById('bg')
-window.addEventListener('scroll', function() {
-	headerBg.style.opacity = 1 - +window.pageYOffset/550+''
-	headerBg.style.top = +window.pageYOffset+'px'
-	headerBg.style.backgroundPositionY = - +window.pageYOffset/2+'px'
-})
